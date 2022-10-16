0e0c3141698a20fef5ae8fc6363933cfa44b5fe2
Hemant Parihar <hepariha@cisco.com>
2022-10-17 02:36:40 +0530

    Dynamically fetching images from database.

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,12 @@ Including another URLconf
 from django.urls import re_path as url
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('travello.urls')),
-    path('', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- travello/views.py
+++ travello/views.py
@@ -5,10 +5,6 @@ from .models import Destination
 # Create your views here.
 def index(request):
 
-    dest1 = Destination(1, "Chennai", "destination_1.jpg", "South City of India", 400, False)
-    dest2 = Destination(2, "Kolkata", "destination_2.jpg", "City of briges.", 800, False)
-    dest3 = Destination(3, "Gujrat", "destination_3.jpg", "Salt city.", 1000, True)
-
-    dests = [dest1, dest2, dest3]
+    dests = Destination.objects.all()
 
     return render(request, 'index copy.html', {'dests': dests})
